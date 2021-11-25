6d6c56dc7373233f5bbaed1f539ca5dac2ac9334
Walied Rashied <w.rashied@icloud.com>
2021-11-25 16:11:37 +0100

    add handleSubmit function

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import Form from './components/DiaryInput/DiaryInput';
+import React, { useState } from 'react';
 import Card from './components/Card/Card';
+import DiaryInput from './components/DiaryInput/DiaryInput';
 
 export default function App(): JSX.Element {
-  return <>
-    <Form />
-    <Card destination={''} memories={''} />
-  </>;
-}
+  const [diaries, setDiaries] = useState([
+    {
+      destination: 'Paris',
+      memories: "it's a beautiful city",
+    },
+  ]);
+
+  function handleSubmit(diary: {
+    destination: string;
+    memories: string;
+  }) {
+    const newDiaries = [...diaries];
+    newDiaries.push(diary);
+    setDiaries(newDiaries);
+  }
+
+  return (
+        <div>
+        <DiaryInput onSubmit={handleSubmit} />
+        {diaries.map((diary) => (
+          <Card
+            destination={diary.destination}
+            memories={diary.memories}
+          />
+        ))}
+      </div>
+  )};
