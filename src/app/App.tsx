fdbb097224851bcf9791bb0924369ed0ed7a4b7b
Walied Rashied <w.rashied@icloud.com>
2021-11-25 12:54:37 +0100

    add Card to App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Form from './components/DiaryInput/DiaryInput';
+import Card from './components/Card/Card';
 
 export default function App(): JSX.Element {
-  return <Form />;
+  return <>
+    <Form />
+    <Card destination={''} memories={''} />
+  </>;
 }
