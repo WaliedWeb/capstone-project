afc86867b3422b52018ea3d1fdfef94e44c2cc8b
Walied Rashied <w.rashied@icloud.com>
2021-12-15 16:46:46 +0100

    fixing navigation

--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -6,20 +6,26 @@ import { NavLink } from 'react-router-dom';
 export default function Navigation(): JSX.Element {
   return (
     <Container>
-      <NavLink to="">
+      <NavLink to="/">
         {' '}
-        <HiHome />{' '}
+        <HiHome size={30} />{' '}
       </NavLink>
-      <NavLink to="">
+      <NavLink to="addMemories">
         {' '}
-        <HiPencilAlt />{' '}
+        <HiPencilAlt size={30} />{' '}
       </NavLink>
-      <NavLink to="">
+      <NavLink to="memories">
         {' '}
-        <HiOutlineBookOpen />{' '}
+        <HiOutlineBookOpen size={30} />{' '}
       </NavLink>
     </Container>
   );
 }
 
-const Container = styled.nav``;
+const Container = styled.nav`
+  display: flex;
+  justify-content: space-around;
+  // position: relative;
+  align-items: flex-end;
+  height: 6vh;
+`;
