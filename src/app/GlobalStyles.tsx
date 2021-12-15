5e4fccc4fbc52140798d17b6d82a85969d29b31c
Walied Rashied <w.rashied@icloud.com>
2021-12-15 16:46:26 +0100

    fixing Global Styles

--- src/app/GlobalStyles.tsx
+++ src/app/GlobalStyles.tsx
@@ -5,6 +5,10 @@ const GlobalStyles = createGlobalStyle`
         box-sizing: border-box;
     }
 
+    h1,h2,h3,h4,h5,h6 {
+        margin: 0;
+    }
+
     body {
     margin: 0;
     font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI',
