8e79aa406b4044d2180e92fe88cd4af31514f2b9
Walied Rashied <w.rashied@icloud.com>
2021-12-15 16:46:12 +0100

    Add Navigation on App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation/Navigation';
 import AddDiary from './Pages/AddDiary';
 import Dashboard from './Pages/Dashboard';
+import LandingPage from './Pages/LandingPage';
 
 export default function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="addMemories" element={<AddDiary />} />
+        <Route path="memories" element={<Dashboard />} />
       </Routes>
+      <Navigation />
     </BrowserRouter>
   );
 }
