9524ad8572d3109bdbf10fb124114dadf9f94d4c
Walied Rashied <w.rashied@icloud.com>
2021-12-16 17:42:25 +0100

    changing Card Style

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -28,27 +28,25 @@ export default function Card({ diary, deleteCard }: CardProps): JSX.Element {
 const CardContainer = styled.article`
   position: relative;
   border-radius: 0.5rem;
-  border: 3px solid #5adeff;
+  border: 3px solid var(--card-bg-dark);
   width: 90%;
   margin: 20px;
-  padding-left: 10px;
-  background-color: #00bdfe;
+  padding: 15px;
+  background-color: var(--card-bg-dark);
 `;
 
 const Destination = styled.h1`
   color: var(--color-text-white);
-  padding: 0.5rem 0.75rem;
   margin: 0;
 `;
 
 const Memories = styled.p`
   color: var(--color-text-lightgreen);
-  padding: 0.5rem 1rem;
-  margin: 0;
 `;
 
-const Date = styled.p`
-  padding: 0.5rem 0.75rem;
-`;
+const Date = styled.p``;
 
-const Image = styled.img``;
+const Image = styled.img`
+  height: 100%;
+  width: 100%;
+`;
